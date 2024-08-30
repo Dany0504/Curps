86add53c95f39280aca83252a185467e32b2bc42
Dany0504 <a223206520@unison.mx>
2024-08-30 08:30:46 -0700

    Validacion Regex

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Escribe el nombre del archivo como argumento");
+            return;
+        }
+
+        String datos = args[0];
+        String ValidacionCurp = "^[A-Z]{1}[AEIOU]{1}[A-Z]{2}\\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])[HM](AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[A-Z0-9]{2}$";
+        Pattern pattern = Pattern.compile(ValidacionCurp);
+
+
+        }
+    }
+}
